d53d8889dd2e7fe04b967376c42e70bcc3d2300a
cherry <130264633+randomcherry@users.noreply.github.com>
2025-12-30 14:35:02 +0000

    Tue, Dec 30, 2026, 2:35 PM +00:00

--- project/app.py
+++ project/app.py
@@ -1,10 +1,5 @@
 import os
-
-# from cs50 import SQL
-from sqlalchemy import create_engine, select
-from sqlalchemy import Table, Column, MetaData
-from sqlalchemy.sql import text
-
+from sqlalchemy import create_engine, text
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 
@@ -15,20 +10,14 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config['SECRET_KEY'] = 'your_very_secret_and_random_key_here'
 Session(app)
-# Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Configure CS50 Library to use SQLite database
-# db = SQL("sqlite:///project.db")
-
-db = 'sqlite:///project.db'
-engine = create_engine(db)
-meta = MetaData()
+# Configure SQLAlchemy engine
+engine = create_engine("sqlite:///project.db", echo=True, future=True)
 conn = engine.connect()
 
 @app.after_request
 def after_request(response):
-    """Ensure responses aren't cached"""
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
@@ -43,26 +32,21 @@ def submitRequest():
     if request.method == "POST":
         name = request.form.get("name")
         food = request.form.get("food")
-        additional = request.form.get("additional")
-        recipe = request.form.get("recipe")
-        anonymous = request.form.get("anonymous")
+        additional = request.form.get("additional") or "NULL"
+        recipe = request.form.get("recipe") or "NULL"
+        anonymous = request.form.get("anonymous") == "anonymous"
 
         if not name:
             return render_template("debug.html", text1="invalid name.")
         elif not food:
             return render_template("debug.html", text1="invalid food.")
         else:
-            if not additional:
-                additional = "NULL"
-            if not recipe:
-                recipe = "NULL"
-            if anonymous == "anonymous":
-                anonymous = True
-            else:
-                anonymous = False
-            db.execute("INSERT INTO requests (name, food, additional, recipe, anonymous) VALUES (?,?,?,?,?)", name, food, additional, recipe, anonymous)
+            conn.execute(
+                text("INSERT INTO requests (name, food, additional, recipe, anonymous) VALUES (:name, :food, :additional, :recipe, :anonymous)"),
+                {"name": name, "food": food, "additional": additional, "recipe": recipe, "anonymous": anonymous}
+            )
+            conn.commit()
             return redirect("/viewRequests")
-
     else:
         return render_template("submitRequest.html")
 
@@ -75,15 +59,16 @@ def viewRequests():
             session[sessionKey] = False
 
         if not session[sessionKey]:
-            newVotes = int((db.execute("SELECT upvotes FROM requests WHERE id=?", requestID))[0]["upvotes"]) + 1
-            db.execute("UPDATE requests SET upvotes=? WHERE id=?", newVotes, requestID)
+            row = conn.execute(text("SELECT upvotes FROM requests WHERE id=:id"), {"id": requestID}).mappings().first()
+            newVotes = int(row["upvotes"]) + 1
+            conn.execute(text("UPDATE requests SET upvotes=:upvotes WHERE id=:id"), {"upvotes": newVotes, "id": requestID})
+            conn.commit()
             session[sessionKey] = True
             return redirect("/viewRequests")
         else:
             return render_template("debug.html", text1="You cannot upvote the same item more than once")
-
     else:
-        requests = db.execute("SELECT id, name, food, upvotes, anonymous FROM requests")
+        requests = conn.execute(text("SELECT id, name, food, upvotes, anonymous FROM requests")).mappings().all()
         return render_template("viewRequests.html", requests=requests)
 
 @app.route("/adminLogIn", methods=["GET", "POST"])
@@ -97,8 +82,10 @@ def adminLogIn():
             elif not password:
                 return render_template("debug.html", text1="invalid password.")
             else:
-                if db.execute("SELECT COUNT(*) AS n FROM admins WHERE name=?", name)[0]["n"] == 1:
-                    if db.execute("SELECT password FROM admins WHERE name=?", name)[0]["password"] == password:
+                count = conn.execute(text("SELECT COUNT(*) AS n FROM admins WHERE name=:name"), {"name": name}).mappings().first()
+                if count["n"] == 1:
+                    pw = conn.execute(text("SELECT password FROM admins WHERE name=:name"), {"name": name}).mappings().first()
+                    if pw["password"] == password:
                         session["admin"] = True
                         return render_template("editRequests.html")
                     else:
@@ -117,7 +104,7 @@ def addAdmin():
         password = request.form.get("password")
         adminKey = request.form.get("adminKey")
         correctAdminKey = "pikafoodAdminHuhu"
-        # return render_template("debug.html", text1=[name, password, adminKey])
+
         if not name:
             return render_template("debug.html", text1="invalid username.")
         elif not password:
@@ -125,10 +112,12 @@ def addAdmin():
         elif not adminKey or adminKey != correctAdminKey:
             return render_template("debug.html", text1="invalid admin key.")
         else:
-            if db.execute("SELECT COUNT(*) AS n FROM admins WHERE name=?", name)[0]["n"] != 0:
+            count = conn.execute(text("SELECT COUNT(*) AS n FROM admins WHERE name=:name"), {"name": name}).mappings().first()
+            if count["n"] != 0:
                 return render_template("debug.html", text1="duplicate username.")
             else:
-                db.execute("INSERT INTO admins (name, password) VALUES (?,?)", name, password)
+                conn.execute(text("INSERT INTO admins (name, password) VALUES (:name, :password)"), {"name": name, "password": password})
+                conn.commit()
                 return redirect("/addAdmin")
     else:
         return render_template("addAdmin.html")
@@ -139,22 +128,27 @@ def editRequests():
         removeRequest = request.form.get("removeRequest")
         finishRequest = request.form.get("finishRequest")
         if removeRequest:
-            db.execute("DELETE FROM requests WHERE id=?", removeRequest)
+            conn.execute(text("DELETE FROM requests WHERE id=:id"), {"id": removeRequest})
+            conn.commit()
         if finishRequest:
-            finishedRequest = db.execute("SELECT id, name, food, additional, recipe, anonymous, upvotes FROM requests WHERE id=?", finishRequest)[0]
+            finishedRequest = conn.execute(text("SELECT id, name, food, additional, recipe, anonymous, upvotes FROM requests WHERE id=:id"), {"id": finishRequest}).mappings().first()
             if finishedRequest:
-                db.execute("INSERT INTO finished (requestID, name, food, additional, recipe, anonymous, upvotes) VALUES (?,?,?,?,?,?,?)", finishedRequest["id"], finishedRequest["name"], finishedRequest["food"], finishedRequest["additional"], finishedRequest["recipe"], finishedRequest["anonymous"], finishedRequest["upvotes"])
-                db.execute("DELETE FROM requests WHERE id=?", finishRequest)
+                conn.execute(
+                    text("INSERT INTO finished (requestID, name, food, additional, recipe, anonymous, upvotes) VALUES (:id, :name, :food, :additional, :recipe, :anonymous, :upvotes)"),
+                    finishedRequest
+                )
+                conn.execute(text("DELETE FROM requests WHERE id=:id"), {"id": finishRequest})
+                conn.commit()
         return redirect("/editRequests")
     elif request.method == "GET":
-        requests = db.execute("SELECT id, name, food, upvotes, anonymous, additional, recipe FROM requests")
-        finished = db.execute("SELECT requestID, food, name, anonymous, upvotes, additional, recipe FROM finished")
-        comments = db.execute("SELECT * FROM comments")
+        requests = conn.execute(text("SELECT id, name, food, upvotes, anonymous, additional, recipe FROM requests")).mappings().all()
+        finished = conn.execute(text("SELECT requestID, food, name, anonymous, upvotes, additional, recipe FROM finished")).mappings().all()
+        comments = conn.execute(text("SELECT * FROM comments")).mappings().all()
         return render_template("editRequests.html", requests=requests, finished=finished, comments=comments)
 
 @app.route("/viewFinished", methods=["GET", "POST"])
 def viewFinished():
-    finished = db.execute("SELECT requestID, name, food, anonymous, upvotes, rating FROM finished")
+    finished = conn.execute(text("SELECT requestID, name, food, anonymous, upvotes, rating FROM finished")).mappings().all()
     return render_template("viewFinished.html", finished=finished)
 
 @app.route("/rateComment", methods=["GET", "POST"])
@@ -163,22 +157,27 @@ def rateComment():
         requestNumber = request.form.get("requestNumber")
         rating = request.form.get("rating")
         comment = request.form.get("comment")
-        if db.execute("SELECT COUNT(*) AS n FROM finished WHERE requestID=?", requestNumber)[0]["n"] == 0:
+        count = conn.execute(text("SELECT COUNT(*) AS n FROM finished WHERE requestID=:id"), {"id": requestNumber}).mappings().first()
+        if count["n"] == 0:
             return render_template("debug.html", text1="invalid request number.")
         elif not rating and not comment:
             return render_template("debug.html", text1="please provide a valid rating or comment.")
         else:
             if rating:
-                currentRating = db.execute("SELECT rating FROM finished WHERE requestID=?", requestNumber)[0]["rating"]
-                ratingCount = db.execute("SELECT ratingCount FROM finished WHERE requestID=?", requestNumber)[0]["ratingCount"]
-                if currentRating == None:
-                    newRating = rating
+                row = conn.execute(text("SELECT rating, ratingCount FROM finished WHERE requestID=:id"), {"id": requestNumber}).mappings().first()
+                currentRating = row["rating"]
+                ratingCount = row["ratingCount"]
+                if currentRating is None:
+                    newRating = int(rating)
                 else:
-                    newRating = round((int(currentRating)*int(ratingCount) + int(rating))/(int(ratingCount)+1), 1)
-                db.execute("UPDATE finished SET rating=? WHERE requestID=?", newRating, requestNumber)
-                db.execute("UPDATE finished SET ratingCount=? WHERE requestID=?", ratingCount+1 ,requestNumber)
+                    newRating = round((int(currentRating) * int(ratingCount) + int(rating)) / (int(ratingCount) + 1), 1)
+                conn.execute(text("UPDATE finished SET rating=:rating, ratingCount=:count WHERE requestID=:id"),
+                             {"rating": newRating, "count": ratingCount + 1, "id": requestNumber})
+                conn.commit()
             if comment:
-                db.execute("INSERT INTO comments (requestID, comment) VALUES (?,?)", requestNumber, comment)
+                conn.execute(text("INSERT INTO comments (requestID, comment) VALUES (:id, :comment)"),
+                             {"id": requestNumber, "comment": comment})
+                conn.commit()
             return redirect("/viewFinished")
     else:
         return render_template("rateComment.html")
